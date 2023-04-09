3cd085766ff5636307830f07286016d3d811e4ea
stric9r <76652407+stric9r@users.noreply.github.com>
2023-04-09 11:23:33 -0400

    Remove example code
    It's in the readme now.

--- rotary_encoders.h
+++ rotary_encoders.h
@@ -9,81 +9,7 @@
 /// Author: Stric Roberts
 /// Date: 4/7/2023
 ///
-/// Usage:
-/// The driver is made in a way to allow multiple encoders to be used
-/// with other GPIO interrupts.
-/// This is meant for bare metal use.
-/// Software or HW debounce up to the developer, not handled here!
-///
-/// One encoder requires two GPIO pins, a DL and CLK for knob turns
-/// This encoder also has a push button (non latching) with requires a 3rd GPIO pin
-///
-/// Example code:
-/// NOTE: Look at define ROTARY_ENCODER_INSTANCES for how many encoders allowed
-///
-/// #include "rotary_encoder.h"
-///
-/// int main(...)
-/// {
-///
-///      uint8_t const volume_instance_id = 0;
-///      rotary_encoder_init(volume_instance_id,
-///                          0,     // min value
-///                          255,   // max value
-///                          true,  // step on max/min values, no roll over
-///                          true); // clockwise turn is positive increment
-///
-///      //mcu specific, psuedo-ish code
-///      mcu_gpio_init(...);
-///
-///      for (;;)
-///      {
-///          rotary_encoder_task();
-///
-///          // Was an interrupt captured and handled?  Update app
-///          if(rotary_encoder_check_event(instance_id))
-///          {
-///              app_update_volume(rotary_encoder_get_knob_value(volume_instance_idinstance_id));
-///          }
-///
-///          // Was the value stepped on?  Ding the user
-///          if(rotary_encoder_check_alert(instance_id))
-///          {
-///              app_alert_user_with_sound();
-///          }
-///      }
-///
-///      return 0;
-/// }
-///
-/// // MCU specific, psuedo-ish code
-/// // Software or HW debounce up to the developer, not handled here
-/// MCU_GPIO_INTERRUPT()
-/// {
-///      // Rotary encoder 0 instance_id CLK pin interrupt flag
-///      // transition low to high
-///      if (PORT0_FLAGS & pinCLK_IF)
-///      {
-///         g_rotary_encoder_instance_num = 0;
-///
-///         // Clock high, DT high == Counter clockwise rotation
-///         // Clock high, DT low == Clockwise rotation
-///         g_rotary_encoder_flags |= mcu_get_pin_state(pinDT) ?
-///                                   ROTARY_ENCODER_FLAG_CDW :
-///                                   ROTARY_ENCODER_FLAG_CW;
-///      }
-///
-///      // Rotary encoder 0 instance_id SW pin interrupt flag
-///      if (PORT0_FLAGS & pinSW_IF)
-///      {
-///         g_rotary_encoder_instance_num = 0;
-///
-///         // Clock high, DT low == Counter clockwise rotation
-///         // Clock high, DT high == Clockwise rotation
-///         g_rotary_encoder_flags |= ROTARY_ENCODER_FLAG_SW;
-///      }
-/// }
-///
+
 #ifndef ROTARY_ENCODERS_H_
 #define ROTARY_ENCODERS_H_
 
@@ -103,7 +29,7 @@
 
 /// Flags that interrupt triggers
 extern volatile uint8_t g_rotary_encoder_flags;
-/// Instance number for flags
+/// Instance number for flags that interrupt triggers
 extern volatile uint8_t g_rotary_encoder_instance_num;
 
 
